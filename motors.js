05852a189dfd8fb3116b44cc5848c3d4573f77cf
Jonny Kalambay <jonnyk20@gmail.com>
2018-10-06 13:45:45 -0700

    Configure i2c

--- motors.js
+++ motors.js
@@ -8,18 +8,18 @@ var board = new five.Board({
   repl: false,
   io: new Raspi()
 });
-
-const motors = {
-  dc1: five.Motor(dcMotorConfigs.m1),
-  dc2: five.Motor(dcMotorConfigs.m2),
-  dc3: five.Motor(dcMotorConfigs.m3),
-  dc4: five.Motor(dcMotorConfigs.m4),
-  s1: new Stepper([7, 0, 2, 3])
-};
+let motors;
 
 board.on('ready', function() {
   console.log('board ready');
   this.i2cConfig();
+  motors = {
+    dc1: five.Motor(dcMotorConfigs.m1),
+    dc2: five.Motor(dcMotorConfigs.m2),
+    dc3: five.Motor(dcMotorConfigs.m3),
+    dc4: five.Motor(dcMotorConfigs.m4),
+    s1: new Stepper([7, 0, 2, 3])
+  };
 });
 
 module.exports = motors;
